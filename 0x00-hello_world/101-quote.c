9661aa45776e6cd340d33f491d9f1982e1d278d3
Tmwangiw <terry.wairimu123@gmail.com>
2023-09-07 18:10:22 -0700

    Print quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Main program
+ *
+ * Return: 1
  */
+
+
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
