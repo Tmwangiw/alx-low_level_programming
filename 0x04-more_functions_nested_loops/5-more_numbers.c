755ea3bd3562fbd5c02377f0385da05984f481bb
Tmwangiw <terry.wairimu123@gmail.com>
2023-09-25 07:15:16 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,17 @@
 
 void more_numbers(void)
 {
-	int i, j:
+	int i, j;
 	
-	for (i = 1; i <= 10; i++)
+	for (i = 1; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j>= 10)
-				_putchar('i');
-		
-			_putchar(j % 10 + '0');
+			if (j >= 10)
+			{
+				_putchar((j /  10)  + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
